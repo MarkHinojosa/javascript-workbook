3a493e748b0638c7a86fafbe52ce3e709f85b1d3
Mark Hinojosa <markhinojosa1212@gmail.com>
2017-11-11 14:58:04 -0600

    edited

--- 04week/functional-javascript/helloWorld.js
+++ 04week/functional-javascript/helloWorld.js
@@ -1,5 +1,38 @@
-function upperCaser(input) {
-  return input.toUpperCase();
+//1
+// function upperCaser(input) {
+//   return input.toUpperCase();
+// }
+//
+// module.exports = upperCaser
+//
+// //2
+// function repeat(operation, num) {
+// function operation(num){
+//   console.log(num)
+//  while(num < 5){
+//    return num = num + 1,
+//    operation(num)
+//  }
+// }
+//
+// operation(num);
+// }
+//
+// repeat(5, 1);
+
+  // Do not remove the line below
+  // module.exports = repeat
+
+
+//3 convert: to an arry#map
+
+function doubleAll(numbers){
+  let blankArr = [];
+  const newArr = numbers.map(function(a){
+    return a = a * 2,
+    blankArr.push(a);
+  })
+  return console.log(blankArr);
 }
 
-module.exports = upperCaser
+   module.exports = doubleAll
