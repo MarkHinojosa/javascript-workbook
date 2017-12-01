7608a821fc2a7d8f8dcd4876c2338330bce94f7e
Mark Hinojosa <markhinojosa1212@gmail.com>
2017-11-30 19:40:57 -0600

    edited

--- 07week/react-towersofhanoi/src/App.js
+++ 07week/react-towersofhanoi/src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
   handleClickEventStack(stack, letter){
     let stackLength = stack.length;
     let lastInStack = stack[stack.length-1];
-    console.log(lastInStack)
+    console.log(this.state[letter] + ' line 17')
     console.log(stackLength)
       if( stack.length > 0 && this.state.popped === 0 ){
          let x = stack.pop();
@@ -23,20 +23,19 @@ class App extends Component {
       if(this.state.popped > 0){
         if(this.state.popped < lastInStack || stackLength === 0){
           let x = this.state.popped;
-          console.log( x + 'this is popped' );
-          stack.push(x);
+          console.log( x + ' this is popped' );
+          this.state[letter].push(x);
           this.setState( { popped : 0 } );
         }
-        // //HOW DO I PASS THE STACK ARGUMENT FROM THE dataStack DIV???
+      }
+      if(this.state.c.length === 4){
+        console.log('you won!')
       }
   }
   render() {
-    let a = a;
-    let b = this.state.b;
-    let c = this.state.c;
     return (
       <div>
-        <div className="dataStack"  onClick={() => this.handleClickEventStack(this.state.a, a)}>
+        <div className="dataStack"  onClick={() => this.handleClickEventStack(this.state.a, 'a')}>
           <h1>A</h1>
           <div className="dataBlock-100">
           </div>
