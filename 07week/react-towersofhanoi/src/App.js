61b7bf402ddf340f8336e44d8507f58a87e4d30c
Mark Hinojosa <markhinojosa1212@gmail.com>
2017-12-01 13:22:44 -0600

    finished towers

--- 07week/react-towersofhanoi/src/App.js
+++ 07week/react-towersofhanoi/src/App.js
@@ -1,59 +1,97 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   state={
-
-      a:[100,75,50,25],
-      b:[],
-      c:[],
-      popped: 0
+        stacks:{
+          a:[100,75,50,25],
+          b:[],
+          c:[]
+        },
+        popped: 0,
+        gameOver: null,
   }
 
-  handleClickEventStack(stack, letter){
-    let stackLength = stack.length;
-    let lastInStack = stack[stack.length-1];
-    console.log(this.state[letter] + ' line 17')
-    console.log(stackLength)
-      if( stack.length > 0 && this.state.popped === 0 ){
-         let x = stack.pop();
-         this.setState( { popped: x } )
-       }
-      if(this.state.popped > 0){
-        if(this.state.popped < lastInStack || stackLength === 0){
-          let x = this.state.popped;
-          console.log( x + ' this is popped' );
-          this.state[letter].push(x);
-          this.setState( { popped : 0 } );
-        }
-      }
-      if(this.state.c.length === 4){
-        console.log('you won!')
-      }
-  }
-  render() {
+  renderBlocks(letter){
+    const callingStacks = this.state.stacks[letter];
+    const test = 'dataBlock';
     return (
-      <div>
-        <div className="dataStack"  onClick={() => this.handleClickEventStack(this.state.a, 'a')}>
-          <h1>A</h1>
-          <div className="dataBlock-100">
-          </div>
-
-          <div className="dataBlock-75">
-          </div>
-          <div className="dataBlock-50">
-          </div>
-          <div className="dataBlock-25">
-          </div>
-        </div>
-        <div className="dataStack" key='b' onClick={() => this.handleClickEventStack(this.state.b, 'b')}>
-        <h1>B</h1>
-        </div>
-        <div className="dataStack" key='c' onClick={() => this.handleClickEventStack(this.state.c, 'c')}>
-        <h1>C</h1>
-        </div>
+      <div>{callingStacks.map(block => <div className={test + '-' + block}></div>)}
       </div>
+    )
+  }
+  renderStacks(){
+    const callingStacks = this.state.stacks;
+    return (
+
+      <div>{Object.keys(callingStacks).map(letter => <div className='dataStack' onClick={() => this.handleClickEventStack(letter)}>{letter} : {this.renderBlocks(letter)}</div>)}</div>
+)
+}
+  win(){
+    return alert('You win!'),
+    this.setState({
+          stacks:{
+            a:[100,75,50,25],
+            b:[],
+            c:[]
+          },
+          popped: 0,
+          gameOver: null
+        })
+  }
+  handleClickEventStack(letter){
+    if(this.state.gameOver !== true){
+    console.log(letter)
+    let stackLength = this.state.stacks[letter].length;
+    let lastInStack = this.state.stacks[letter][stackLength - 1];
+    console.log(lastInStack);
+
+    if( stackLength && this.state.popped === 0 ){
+      let storedPopped = this.state.stacks[letter].pop();
+      this.setState( { popped: storedPopped } )
+    }
+    if(this.state.popped > 0){
+      if(this.state.popped < lastInStack || stackLength === 0){
+        let storedPopped = this.state.popped;
+        console.log('this is stored ', storedPopped)
+        this.state.stacks[letter].push(storedPopped);
+        this.setState( { popped : 0 } );
+      }
+    }
+    if(this.state.stacks.c.length === 4){
+      return this.setState( { gameOver: true } ),
+      this.win();
+    }
+
+  }
+}
+  render() {
+
+
+    return (
+        this.renderStacks()
+      //
+      // <div>
+      //
+      //   <div className="dataStack"  onClick={() => this.handleClickEventStack(this.state.stacks.a, 'a')}>
+      //     <h1>A</h1>
+      //     <div className="dataBlock-100">
+      //     </div>
+      //
+      //     <div className="dataBlock-75">
+      //     </div>
+      //     <div className="dataBlock-50">
+      //     </div>
+      //     <div className="dataBlock-25">
+      //     </div>
+      //   </div>
+      //   <div className="dataStack" key='b' onClick={() => this.handleClickEventStack(this.state.b, 'b')}>
+      //   <h1>B</h1>
+      //   </div>
+      //   <div className="dataStack" key='c' onClick={() => this.handleClickEventStack(this.state.c, 'c')}>
+      //   <h1>C</h1>
+      //   </div>
+      // </div>
     );
   }
 }
